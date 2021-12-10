951eb1693638da43f745edd48b876f099ba07583
liuyongsheng <lysxx717@gmail.com>
2021-12-10 17:40:51 +0800

    format code

--- src/common.h
+++ src/common.h
@@ -3,39 +3,36 @@
 
 static const unsigned int UNSELECTED = 0;
 
-enum class Difficulty : int
-{
-    EASY = 1,
-    NORMAL,
-    HARD
+enum class Difficulty : int {
+  EASY = 1,
+  NORMAL,
+  HARD
 };
 
-enum class State : int
-{
-    INITED = 0,
-    ERASED,
+enum class State : int {
+  INITED = 0,
+  ERASED,
 };
 
 using point_t = struct point_t {
-    int x;
-    int y;
+  int x;
+  int y;
 };
 
 using point_value_t = struct point_value_t {
-    int value;
-    State state;
+  int value;
+  State state;
 };
 
-class CPointSort
-{
-  public:
-    bool operator()(const point_t &lhs, const point_t &rhs) const
-    {
-        if ((lhs.x == rhs.x) && (lhs.y == rhs.y))
-            return false;
-        else
-            return true;
+class CPointSort {
+ public:
+  bool operator()(const point_t &lhs, const point_t &rhs) const {
+    if ((lhs.x == rhs.x) && (lhs.y == rhs.y)) {
+      return false;
+    } else {
+      return true;
     }
+  }
 };
 
 #endif
--- src/test.cpp
+++ src/test.cpp
@@ -5,15 +5,12 @@
 using namespace std;
 
 //生成棋盘,10s
-void test_case1()
-{
-    CScene scene;
-
-    time_t begin, end;
-    time(&begin);
-    scene.generate();
-    time(&end);
-
-    scene.show();
-    cout << end - begin << endl;
+void test_case1() {
+  CScene scene;
+  time_t begin, end;
+  time(&begin);
+  scene.generate();
+  time(&end);
+  scene.show();
+  cout << end - begin << endl;
 }
